c7059286105b171757c8b56c7a0dbde107ba157f
Martin <martin.birisic@gmail.com>
2024-09-18 22:29:42 +0200

    refactor(component): updated the login form validation to use Reactive Forms

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,39 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  public form = {
-    username: '',
-    password: '',
-    errors: {
-      username: '',
-      password: '',
-    },
-  };
+  public loginForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5)
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[A-Z])(?=.*\d)/) //at least 1 uppercase and 1 number
+      ])
+    });
+  }
 
   public onSubmit(): void {
-    this.form.errors = { username: '', password: ''};
-
-    //validate username
-    if (!this.form.username) {
-      this.form.errors.username = 'Username is required.';
-    } else if (this.form.username.length < 5) {
-      this.form.errors.username = 'Username must be at least 5 characters long.';
-    }
-
-    //validate password
-    if (!this.form.password) {
-      this.form.errors.password = 'Password is required.';
-    } else if (!this.isValidPassword(this.form.password)) {
-      this.form.errors.password = 'Password must be at least 8 characters long, contain at least 1 uppercase letter and 1 number.';
-    }
-
-    if (!this.form.errors.username && !this.form.errors.password) {
-      console.log('Form Submitted:', this.form);
+    if (this.loginForm.valid) {
+      console.log('Form Submitted:', this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
     }
   }
 
-  private isValidPassword(password: string): boolean {
-    const passwordPattern = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    return passwordPattern.test(password);
-  }
+  get username() { return this.loginForm.get('username'); }
+  get password() { return this.loginForm.get('password'); }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './components/login/login.component';
-import { SharedModule } from '../shared/shared.module';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,7 +13,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     LoginRoutingModule,
-    FormsModule
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
